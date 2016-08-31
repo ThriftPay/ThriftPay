94542e27889947d1f648c4d4d8219b46ad494561
Robert Campbell <Robert Campbell>
2016-08-30 22:48:56 -0500

    Added dotnet restore command to publish scripts

--- publish_account.sh
+++ publish_account.sh
@@ -3,6 +3,7 @@
 # publish
 cd src/ThriftPay.Account
 supervisorctl stop account.thriftpay.co
+dotnet restore
 dotnet publish -r coreclr -o /var/www/aspnet/ThriftPay.Account
 supervisorctl start account.thriftpay.co
 cd ../../
--- publish_api.sh
+++ publish_api.sh
@@ -3,6 +3,7 @@
 # publish
 cd src/ThriftPay.API
 supervisorctl stop api.thriftpay.co
+dotnet restore
 dotnet publish -r coreclr -o /var/www/aspnet/ThriftPay.API
 supervisorctl start api.thriftpay.co
 cd ../../
