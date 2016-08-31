a090cf87697f8a63dc9a06a5651258ce884de03d
Robert Campbell <Robert Campbell>
2016-08-30 22:46:29 -0500

    Added publish scripts to solution root

--- publish_account.sh
+++ publish_account.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # publish
+cd src/ThriftPay.Account
 supervisorctl stop account.thriftpay.co
 dotnet publish -r coreclr -o /var/www/aspnet/ThriftPay.Account
 supervisorctl start account.thriftpay.co
+cd ../../
--- publish_api.sh
+++ publish_api.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # publish
+cd src/ThriftPay.API
 supervisorctl stop api.thriftpay.co
 dotnet publish -r coreclr -o /var/www/aspnet/ThriftPay.API
 supervisorctl start api.thriftpay.co
+cd ../../
